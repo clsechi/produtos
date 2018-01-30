49f10012604dcab648a405e4894be041e6df9fd9
Italo e Thiago M <pair06@campuscode.com.br>
2018-01-30 16:32:25 -0200

    Correções Rubocop

--- spec/features/user_see_product_category_spec.rb
+++ spec/features/user_see_product_category_spec.rb
@@ -1,36 +1,39 @@
 require 'rails_helper'
+
 feature 'product_category' do
-   scenario 'success' do
-     product_category = ProductCategory.create(name: 'Hospedagem',
-                                               description: 'Domínio grátis e sites ilimitados')
+  scenario 'success' do
+    product_category = ProductCategory.create(name: 'Hospedagem',
+                                              description: 'Domínio grátis e
+                                              sites ilimitados')
 
-     visit root_path
-     click_on('Categoria')
+    visit root_path
+    click_on('Categoria')
 
-     expect(page).to have_content(product_category.name)
-     expect(page).to have_content(product_category.description)
-   end
+    expect(page).to have_content(product_category.name)
+    expect(page).to have_content(product_category.description)
+  end
 
-   scenario 'show error message when dont hanve category' do
+  scenario 'show error message when dont hanve category' do
+    visit root_path
+    click_on('Categoria')
 
-     visit root_path
-     click_on('Categoria')
+    expect(page).to have_content('Não há categorias cadastradas!')
+  end
 
-     expect(page).to have_content('Não há categorias cadastradas!')
-   end
+  scenario 'show two categories' do
+    product_category = ProductCategory.create(name: 'Hospedagem',
+                                              description: 'Domínio grátis e
+                                             sites ilimitados')
+    other_product_category = ProductCategory.create(name: 'Email',
+                                                    description: 'Contas
+                                                    ilimitadas')
 
-   scenario 'show two categories' do
-     product_category = ProductCategory.create(name: 'Hospedagem',
-                                               description: 'Domínio grátis e sites ilimitados')
-     other_product_category = ProductCategory.create(name: 'Email',
-                                               description: 'Contas ilimitadas')
+    visit root_path
+    click_on('Categoria')
 
-     visit root_path
-     click_on('Categoria')
-
-     expect(page).to have_content(product_category.name)
-     expect(page).to have_content(product_category.description)
-     expect(page).to have_content(other_product_category.name)
-     expect(page).to have_content(other_product_category.description)
-   end
+    expect(page).to have_content(product_category.name)
+    expect(page).to have_content(product_category.description)
+    expect(page).to have_content(other_product_category.name)
+    expect(page).to have_content(other_product_category.description)
+  end
 end
