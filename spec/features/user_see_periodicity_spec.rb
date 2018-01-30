abc63c6b4a16818c98a104916a27a5d8c52c354c
Luiz e Davi <pair05@campuscode.com.br>
2018-01-30 16:22:00 -0200

    cenario ver duas periodicidades na tela de listagem
    criado cenario de ver duas periodicidades

--- spec/features/user_see_periodicity_spec.rb
+++ spec/features/user_see_periodicity_spec.rb
@@ -3,7 +3,7 @@ require 'rails_helper'
 feature 'User can see peridocity' do
   scenario 'sucessfuly' do
 
-    periodicity = Periodicity.create(name: 'Mensal' , period: 3)
+    periodicity = Periodicity.create(name: 'Mensal' , period: 1)
 
     visit root_path
     click_on('Periodicidade')
@@ -16,4 +16,19 @@ feature 'User can see peridocity' do
     expect(page).not_to have_content('Mensal')
     expect(page).to have_content('Não existe nenhuma periodicidade cadastrada')
   end
+
+  scenario 'and see two periodicity' do
+
+    first_periodicity = Periodicity.create(name: 'Mensal' , period: 1)
+    second_periodicity = Periodicity.create(name: 'Trimestral' , period: 3)
+
+    visit periodicities_path
+
+    expect(page).to have_content(first_periodicity.name)
+    expect(page).to have_content(first_periodicity.period)
+    expect(page).to have_content(second_periodicity.name)
+    expect(page).to have_content(second_periodicity.period)
+
+  end
+
 end
