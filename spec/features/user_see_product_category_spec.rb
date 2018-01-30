124204776ff4be2044678e8e68a7a484f5863fa8
Italo e Thiago M <pair06@campuscode.com.br>
2018-01-30 15:10:31 -0200

    Exibe notice
    Verifica variável vazia
    Cria flash notice no product_category/index

--- spec/features/user_see_product_category_spec.rb
+++ spec/features/user_see_product_category_spec.rb
@@ -1,11 +1,19 @@
 require 'rails_helper'
 feature 'product_category' do
-   scenario 'success'do
+   scenario 'success' do
      product_category = ProductCategory.create(name: 'Hospedagem', description: 'Domínio grátis e sites ilimitados')
 
      visit root_path
      click_on('Categoria')
 
      expect(page).to have_content(product_category.name)
+   end
+
+   scenario 'show error message when dont hanve category' do
+
+     visit root_path
+     click_on('Categoria')
+
+     expect(page).to have_content('Não há categorias cadastradas!')
    end
 end
