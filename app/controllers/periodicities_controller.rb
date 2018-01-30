09d99a27fb2b7fd6a842584dd07edad41cbc3176
Luiz e Davi <pair05@campuscode.com.br>
2018-01-30 16:14:21 -0200

    Scenario sem nenhuma periodicidade cadastrada
    Verificar se esta retornando alguma periodicidade, se sim imprimi se não
    imprimi mensagem

--- app/controllers/periodicities_controller.rb
+++ app/controllers/periodicities_controller.rb
@@ -1,5 +1,9 @@
 class PeriodicitiesController < ApplicationController
   def index
     @periodicities = Periodicity.all
+    if @periodicities.empty?
+      flash.now[:notice] = 'Não existe nenhuma periodicidade cadastrada'
+      render :index
+    end
   end
 end
--- spec/features/user_see_periodicity_spec.rb
+++ spec/features/user_see_periodicity_spec.rb
@@ -1,6 +1,6 @@
 require 'rails_helper'
 
-feature 'User can see peridocity ' do
+feature 'User can see peridocity' do
   scenario 'sucessfuly' do
 
     periodicity = Periodicity.create(name: 'Mensal' , period: 3)
@@ -10,5 +10,10 @@ feature 'User can see peridocity ' do
 
     expect(page).to have_content(periodicity.name)
     expect(page).to have_content(periodicity.period)
+  end
+  scenario 'and see nothing if periodicities table to be blank' do
+    visit periodicities_path
+    expect(page).not_to have_content('Mensal')
+    expect(page).to have_content('Não existe nenhuma periodicidade cadastrada')
   end
 end
