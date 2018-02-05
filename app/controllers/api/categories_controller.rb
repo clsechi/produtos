d03138ee329ad34c7196b624e8ad6a26edf0677b
Luiz e Davi <pair05@campuscode.com.br>
2018-02-05 16:05:06 -0200

    Ajustes no controlador e teste de categoria

--- app/controllers/api/categories_controller.rb
+++ app/controllers/api/categories_controller.rb
@@ -3,7 +3,7 @@ module Api
     def index
       @categories = ProductCategory.all
       return render json: @categories, status: 200 unless @categories.empty?
-      render json: { message: 'Nenhuma categoria encontrada', status: 404 }
+      render json: { message: 'Nenhuma categoria encontrada' }, status: 404
     end
   end
 end
--- spec/requests/api_categories_spec.rb
+++ spec/requests/api_categories_spec.rb
@@ -16,7 +16,7 @@ describe 'Category API', type: :request do
     data = JSON.parse(response.body)
 
     expect(data['message']).to eq 'Nenhuma categoria encontrada'
-    expect(response.status).to eq 200
+    expect(response.status).to eq 404
   end
 
   it 'and see a list of categories ' do
