04f3bf5bf7498e6550c9bf4be178ecf56d659ec8
Luiz e Davi <pair05@campuscode.com.br>
2018-02-06 16:58:31 -0200

    remove @ do controller de categorios

--- app/controllers/api/categories_controller.rb
+++ app/controllers/api/categories_controller.rb
@@ -1,15 +1,15 @@
 module Api
   class CategoriesController < ApplicationController
     def index
-      @categories = ProductCategory.all
-      @categories = @categories.as_json(except: [:created_at, :updated_at])
-      return render json: { categories: @categories }, status: 200 unless @categories.empty?
+      categories = ProductCategory.all
+      json = categories.as_json(except: [:created_at, :updated_at])
+      return render json: { categories: json }, status: 200 unless categories.empty?
       render json: { message: 'Nenhuma categoria encontrada' }, status: 404
     end
     def show
-      @category = ProductCategory.find(params[:id])
-      @category = @category.as_json(except: [:created_at, :updated_at])
-      return render json: {categories: @category}, status: 200 unless @category.nil?
+      category = ProductCategory.find(params[:id])
+      json = category.as_json(except: [:created_at, :updated_at])
+      return render json: {categories: json}, status: 200 unless category.nil?
       render json: { message: 'Nenhuma categoria encontrada' }, status: 404
     end
   end
