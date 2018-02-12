0f1178f99181ab35180fd2f24df8387ed890dd31
Davi, Luiz e Thiago M <pair05@campuscode.com.br>
2018-02-11 22:16:42 -0200

    Ajustes no Locastyle e UX da aplicação

--- app/models/periodicity.rb
+++ app/models/periodicity.rb
@@ -2,4 +2,6 @@ class Periodicity < ApplicationRecord
   has_many :plan_prices, dependent: :destroy
   validates :name, :period, presence: true
   validates :period, numericality: { only_integer: true }
+  validates :period, numericality: { greater_than_or_equal_to: 1,
+                                     less_than_or_equal_to: 12 }
 end
