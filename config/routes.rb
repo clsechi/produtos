c607aa8c4703d4f4cc6f6d4fb3680e21ead96e2f
Carlos, Thiago M, Davi e Luiz <pair06@campuscode.com.br>
2018-02-13 00:50:50 -0200

    Corrige bug ao enviar preços pela API

--- config/routes.rb
+++ config/routes.rb
@@ -30,11 +30,5 @@ Rails.application.routes.draw do
       resources :plan_prices, only: [:index]
     end
     resources :periodicities, only:[:index]
-
-    resources :product_plans, only:[:index, :show] do
-      member do
-        get 'price', to: 'product_plans#price'
-      end
-    end
   end
 end
